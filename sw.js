9cc4145cb8987b0fed71e9ad17bd769acd48e504
ztexpert-hash <zt.expert@gmail.com>
2026-06-02 11:57:00 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fdmu-zhytomyr-v2_6-1';
+const CACHE = 'fdmu-zhytomyr-v2_7-1';
 const FILES = ['./index.html', './manifest.json', './calc_data.json', './logo.jpg', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
