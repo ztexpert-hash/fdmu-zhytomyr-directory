c456d62742ce7d7ea67bdcf93522e49aebc839a5
ztexpert-hash <zt.expert@gmail.com>
2026-08-02 19:38:13 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fdmu-zhytomyr-v11_3-cache';
+const CACHE = 'fdmu-zhytomyr-v11_6-cache';
 const STATIC_FILES = ['./manifest.json','./logo.jpg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(STATIC_FILES)));
@@ -10,7 +10,7 @@ self.addEventListener('activate', event => {
 });
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
-  const isData = url.pathname.endsWith('/calc_data.json') || url.pathname.endsWith('/index.html') || url.pathname.endsWith('/README.txt') || url.pathname.endsWith('/sw.js') || url.pathname.endsWith('/');
+  const isData = url.pathname.endsWith('/calc_data.json') || url.pathname.endsWith('/update_summary.json') || url.pathname.endsWith('/index.html') || url.pathname.endsWith('/README.txt') || url.pathname.endsWith('/sw.js') || url.pathname.endsWith('/');
   if (isData) {
     event.respondWith(fetch(event.request, {cache:'reload'}).catch(() => caches.match(event.request)));
     return;
